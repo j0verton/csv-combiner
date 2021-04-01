7fbcd3f69b0bada5648d9d34b847fa34f35bde73
j0verton <overtonje@gmail.com>
2021-04-01 16:37:06 -0500

    have two tests set up for parsecsv

--- csv-combiner.js
+++ csv-combiner.js
@@ -3,7 +3,7 @@ import { filterOutCSVFilesFromArgumentArray, changeHeaderToFileName } from "./da
 import { parseCSVFile, parseAllCSVsAsynchronously, parseHeader, outputToCSV } from "./parsing-functions.js";
 
 async function main(arr) {
-
+    console.log(arr)
     // remove anything without a .csv extension from the array of arguments
     const csvFileArray = filterOutCSVFilesFromArgumentArray(arr);
     //need to add some logic for cases when no csv is passed
--- test/parsing-function-tests.js
+++ test/parsing-function-tests.js
@@ -1,5 +1,8 @@
 import assert from 'assert'
 import { parseCSVFile } from '../parsing-functions.js';
+
+import path from 'path'
+
 describe('Parsing Function Tests', function () {
     describe('parse CSV File', function () {
 
@@ -10,16 +13,21 @@ describe('Parsing Function Tests', function () {
         // should skip lines that would throw an error
         // should throw an error if no data is returned 
 
+        let workingCSVPath = './test/test-fixtures/working-five-line.csv';
+        let oddDataCSVPath = './test/test-fixtures/odd-data-five-line.csv';
+        let blankCSVPath = './test/test-fixtures/blank.csv';
 
-
-        let workingCSVPath = './test-fixtures/working-five-line.csv'
-        let workingCSVExpectedResult = [["one", "Satchels"], ["two", "Purses"], ["three", "Purses"], ["four", "Wallets"]]
-
-        it('should skip the header and return an array formatted from the data in the .csv file', async function () {
+        let workingCSVExpectedResult = [['one', 'Satchels', 'working-five-line.csv'], ['two', 'Purses', 'working-five-line.csv'], ['three', 'Purses', 'working-five-line.csv'], ['four', 'Wallets', 'working-five-line.csv']]
+        let oddDataCSVExpectedResult = [["one", "Blouses", "odd-data-five-line.csv"], ["two", "Cardigans", "odd-data-five-line.csv"], ["three", "Capris", "odd-data-five-line.csv"], ["four", "\"Gingham\" Shorts", "odd-data-five-line.csv"]]
+        it('should return an array of the data in the .csv file without the header and with a new column for the filename', async function () {
             let response = await parseCSVFile(workingCSVPath)
-            assert.strictEqual(response, workingCSVExpectedResult);
+            assert.deepEqual(response, workingCSVExpectedResult);
         });
 
+        it('should return an array containing the strangely formatted data', async function () {
+            let response = await parseCSVFile(oddDataCSVPath)
+            assert.deepEqual(response, oddDataCSVExpectedResult);
+        });
         //         it('should not throw an InputError when passed an array with 3 .csv files', function () {
         //             assert.doesNotThrow(() => { filterOutCSVFilesFromArgumentArray(arrayOne) }, InputError);
         //         });
