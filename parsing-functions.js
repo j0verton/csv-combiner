7755eae2c3e9a8ac09c90e8827633c04e843fa1c
j0verton <overtonje@gmail.com>
2021-04-01 13:10:57 -0500

    trying to clean up the data from clothing.csv during the stream, have it to the point where the data is saved but there are 4 extra quotes in it

--- parsing-functions.js
+++ parsing-functions.js
@@ -11,10 +11,12 @@ export const parseCSVFile = async (csvFilePath) => {
         fs.createReadStream(csvFilePath)
             .pipe(csv(
                 {
-                    // 
+                    // this escape preserves the specific data provided in the fixtures,
+                    // though its not a perfect solution even for that
+                    escape: '\\',
                     skip_lines_with_error: true,
 
-                    // I skipped the headers in an attempt to make the application faster when combining later
+                    // I skipped parsing the headers in an attempt to make the application faster when combining later
                     from: 2
                 }
             ))
@@ -22,6 +24,11 @@ export const parseCSVFile = async (csvFilePath) => {
                 data.push(fileName);
                 results.push(data);
             })
+            // i had included this on error to notify the user at the top of the document 
+            // if the combination process skipped any data, but it doesn't work while skip... is true
+            // i would try and impliment this before I'd condsider the application ready 
+            // so the user is aware of any data loss
+            // .on('error', () => { console.log(`error, skipped 1 line in ${fileName}`) })
             .on('end', () => {
                 resolve(results)
             });
@@ -32,6 +39,10 @@ export const parseAllCSVsAsynchronously = async (csvFileArray, parsingFunction) 
     return Promise.all(csvFileArray.map(item => parsingFunction(item)))
 }
 
+export const checkForParsingErrors = () => {
+
+
+}
 export const parseHeader = async (csvFilePath) => {
     const fileName = convertPathToFileName(csvFilePath)
     let results = []
