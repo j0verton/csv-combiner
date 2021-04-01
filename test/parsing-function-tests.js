e18f75fbc9a8c5dbf283d843b71eb074713f25c3
j0verton <overtonje@gmail.com>
2021-04-01 16:57:27 -0500

    taking a break

--- test/parsing-function-tests.js
+++ test/parsing-function-tests.js
@@ -6,19 +6,18 @@ import NoContentError from '../errors/no-content-error.js';
 
 describe('Parsing Function Tests', function () {
     describe('parse CSV File', function () {
-
-
-
         // tests remaining
-        // should skip lines that would throw an error
         // should throw an error if no data is returned 
 
         let workingCSVPath = './test/test-fixtures/working-five-line.csv';
         let oddDataCSVPath = './test/test-fixtures/odd-data-five-line.csv';
         let blankCSVPath = './test/test-fixtures/blank.csv';
+        let errorCSVPath = './test/test-fixtures/with-error.csv';
 
         let workingCSVExpectedResult = [['one', 'Satchels', 'working-five-line.csv'], ['two', 'Purses', 'working-five-line.csv'], ['three', 'Purses', 'working-five-line.csv'], ['four', 'Wallets', 'working-five-line.csv']]
         let oddDataCSVExpectedResult = [["one", "Blouses", "odd-data-five-line.csv"], ["two", "Cardigans", "odd-data-five-line.csv"], ["three", "Capris", "odd-data-five-line.csv"], ["four", "\"Gingham\" Shorts", "odd-data-five-line.csv"]]
+        let withErrorCSVExpectedResult = [["two", "Cardigans", "with-error.csv"], ["three", "Capris", "with-error.csv"], ["four", "\"Gingham\" Shorts", "with-error.csv"]]
+
         it('should return an array of the data in the .csv file without the header and with a new column for the filename', async function () {
             let response = await parseCSVFile(workingCSVPath)
             assert.deepEqual(response, workingCSVExpectedResult);
@@ -29,8 +28,21 @@ describe('Parsing Function Tests', function () {
             assert.deepEqual(response, oddDataCSVExpectedResult);
         });
 
-        it('should throw an NoContentError when passed an array no .csv files', function () {
-            assert.throws(async () => { await parseCSVFile(oddDataCSVPath) }, NoContentError);
+        it('should remove a line that would throw an error', async function () {
+            let response = await parseCSVFile(errorCSVPath)
+            assert.deepEqual(response, withErrorCSVExpectedResult);
         });
+
+        it('should throw an NoContentError when passed an empty .csv file', function () {
+            assert.throws(async () => { await parseCSVFile(blankCSVPath) }, NoContentError('blank.csv'));
+        });
+
+
+
+
+
     });
+
+    // try for testing stdout
+    // assert.deepEqual(inspect.output, [ "foo\n" ]);
 });
