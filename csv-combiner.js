d76c1c47bb68c24121092f70abfc956032ba1a8b
j0verton <overtonje@gmail.com>
2021-04-01 23:04:01 -0500

    cleaned up the comments in main

--- csv-combiner.js
+++ csv-combiner.js
@@ -5,13 +5,12 @@ import { parseCSVFile, parseAllCSVsAsynchronously, parseHeader, outputToCSV } fr
 async function main(arr) {
     // remove anything without a .csv extension from the array of arguments
     const csvFileArray = filterOutCSVFilesFromArgumentArray(arr);
-    //need to add some logic for cases when no csv is passed
 
     const data = await parseAllCSVsAsynchronously(csvFileArray, parseCSVFile);
 
     //parse headers
     const headers = await parseAllCSVsAsynchronously(csvFileArray, parseHeader);
-
+    //output header then data to avoid having to interate or manipulate the data arrays
     outputToCSV(headers, data)
 }
 
